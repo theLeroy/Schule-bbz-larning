68ebe736135921bf5977b85c7e318cf71e72978c
Leroy <4c65726f79@gmail.com>
2019-01-28 14:09:09 +0100

    con to db

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
   </head>
   <body>
     <h1>Personen</h1>
+    <?php
+      require('phpModules/functions.php');
+
+       $result = mysqli_query($conn, "SELECT * FROM persons;");
+
+       $row = mysqli_fetch_array($result, MYSQLI_NUM);
+       print_r($row);
+
+
+     ?>
   </body>
 </html>
