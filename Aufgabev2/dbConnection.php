b77ae540b5e2efdda28995b1f1d9ec3630cec7f6
Leroy <4c65726f79@gmail.com>
2019-02-18 13:42:38 +0100

    soooo lw

---
+++ Aufgabev2/dbConnection.php
@@ -0,0 +1,17 @@
+<?php
+
+ini_set('display_errors', 'On'); ini_set('html_errors', 0); error_reporting(-1); //Show errors
+
+//Php db login daten
+$host = "mdm2016.bbz.cloud";
+$port = 21;
+$database = "d_php_intro";
+$username = "mdm2016d";
+$password = "e0ba21337840eecef285844f892946f7";
+$conn = mysqli_connect($host, $username, $password, $database, $port);
+if (!$conn) { //Sql Verbindung Test
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+ ?>
--- Aufgabev2/send.php
+++ Aufgabev2/send.php
@@ -1,35 +1,22 @@
 <?php
 
-ini_set('display_errors', 'On'); ini_set('html_errors', 0); error_reporting(-1);
+require 'dbConnection.php'; // Läd das File
 
-$host = "mdm2016.bbz.cloud";
-$port = 21;
-$database = "d_php_intro";
-$username = "mdm2016d";
-$password = "e0ba21337840eecef285844f892946f7";
-$conn = mysqli_connect($host, $username, $password, $database, $port);
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
- echo $_POST["name"];
- echo $_POST["vorname"];
-
-$name = mysqli_real_escape_string($conn, $_POST["name"]);
+$name = mysqli_real_escape_string($conn, $_POST["name"]); //Escape um sql injectons zu umgehen
 $vorname = mysqli_real_escape_string($conn, $_POST["vorname"]);
 $PLZ = mysqli_real_escape_string($conn, $_POST["PLZ"]);
 $strasse = mysqli_real_escape_string($conn, $_POST["strasse"]);
 $nummer = mysqli_real_escape_string($conn, $_POST["nummer"]);
 
-$sql = "INSERT INTO persons (name, vorname, PLZ, nummer, strasse) VALUES ('". $vorname ."', '". $name ."', '". $PLZ ."', '".$nummer."', '". $strasse. "')";
+$sql = "INSERT INTO persons (name, vorname, PLZ, nummer, strasse) VALUES ('". $vorname ."', '". $name ."', '". $PLZ ."', '".$nummer."', '". $strasse. "')"; //Sql abfrage -> bestimmt was pasiert
 if(mysqli_query($conn, $sql)){
-    echo "Records inserted successfully.";
-    ?>
-    <h1>es het klappet!</h1>
+    echo "<h1>es het klappet!<h1>"; //Feedback
 
-     <?php
-} else{
+} else{ //Error handling
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
+
+
 
 
 
